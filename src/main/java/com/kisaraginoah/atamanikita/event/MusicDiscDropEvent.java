77e29aba380b71ab7b1a177d085c2e547d1ad622
kisaraginoah <kouta19501821@gmail.com>
2025-05-14 10:36:32 +0900

    細かい修正

--- src/main/java/com/kisaraginoah/atamanikita/event/MusicDiscDropEvent.java
+++ src/main/java/com/kisaraginoah/atamanikita/event/MusicDiscDropEvent.java
@@ -9,7 +9,6 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 
 import java.util.List;
