c48bd346e98a453526deb1e3ac9cc4f058189e72
kisaraginoah <kouta19501821@gmail.com>
2025-05-15 16:32:02 +0900

    UniversalToolに各ツールの機能を追加

--- src/main/java/com/kisaraginoah/atamanikita/item/tool/UniversalTool.java
+++ src/main/java/com/kisaraginoah/atamanikita/item/tool/UniversalTool.java
@@ -1,23 +1,60 @@
 package com.kisaraginoah.atamanikita.item.tool;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.component.Tool;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.neoforged.neoforge.common.ItemAbilities;
+import net.neoforged.neoforge.common.ItemAbility;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class UniversalTool extends Item {
+
+    protected static final Map<Block, BlockState> FLATTENABLES = Maps.newHashMap(
+            new ImmutableMap.Builder()
+                    .put(Blocks.GRASS_BLOCK, Blocks.DIRT_PATH.defaultBlockState())
+                    .put(Blocks.DIRT, Blocks.DIRT_PATH.defaultBlockState())
+                    .put(Blocks.PODZOL, Blocks.DIRT_PATH.defaultBlockState())
+                    .put(Blocks.COARSE_DIRT, Blocks.DIRT_PATH.defaultBlockState())
+                    .put(Blocks.MYCELIUM, Blocks.DIRT_PATH.defaultBlockState())
+                    .put(Blocks.ROOTED_DIRT, Blocks.DIRT_PATH.defaultBlockState())
+                    .build()
+    );
+
     public UniversalTool() {
         super(new Properties().stacksTo(1).durability(100000).component(DataComponents.TOOL, new Tool(List.of(Tool.Rule.minesAndDrops(TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("minecraft", "mineable/pickaxe")), 20.0F), Tool.Rule.minesAndDrops(TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("minecraft", "mineable/axe")), 20.0F), Tool.Rule.minesAndDrops(TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("minecraft", "mineable/shovel")), 20.0F)), 20.0F, 1)).rarity(Rarity.EPIC));
     }
@@ -30,5 +67,154 @@ public class UniversalTool extends Item {
     @Override
     public boolean isEnchantable(ItemStack stack) {
         return false;
+    }
+
+    @Override
+    public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity entityLiving) {
+        if (!level.isClientSide && !state.is(BlockTags.FIRE)) {
+            stack.hurtAndBreak(1, entityLiving, EquipmentSlot.MAINHAND);
+        }
+
+        return state.is(BlockTags.LEAVES)
+                || state.is(Blocks.COBWEB)
+                || state.is(Blocks.SHORT_GRASS)
+                || state.is(Blocks.FERN)
+                || state.is(Blocks.DEAD_BUSH)
+                || state.is(Blocks.HANGING_ROOTS)
+                || state.is(Blocks.VINE)
+                || state.is(Blocks.TRIPWIRE)
+                || state.is(BlockTags.WOOL);
+    }
+
+    @Override
+    public InteractionResult interactLivingEntity(ItemStack stack, Player player, LivingEntity entity, net.minecraft.world.InteractionHand hand) {
+        if (entity instanceof net.neoforged.neoforge.common.IShearable target) {
+            BlockPos pos = entity.blockPosition();
+            boolean isClient = entity.level().isClientSide();
+            if (target.isShearable(player, stack, entity.level(), pos)) {
+                List<ItemStack> drops = target.onSheared(player, stack, entity.level(), pos);
+                if (!isClient) {
+                    for(ItemStack drop : drops) {
+                        target.spawnShearedDrop(entity.level(), pos, drop);
+                    }
+                }
+                entity.gameEvent(GameEvent.SHEAR, player);
+                if (!isClient) {
+                    stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
+                }
+                return InteractionResult.sidedSuccess(isClient);
+            }
+        }
+        return InteractionResult.PASS;
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+        BlockPos blockPos = context.getClickedPos();
+        Player player = context.getPlayer();
+        BlockState blockstate = level.getBlockState(blockPos);
+        BlockState blockstate1 = blockstate.getToolModifiedState(context, ItemAbilities.SHEARS_TRIM, false);
+        BlockState toolModifiedState = level.getBlockState(blockPos).getToolModifiedState(context, ItemAbilities.HOE_TILL, false);
+        Pair<Predicate<UseOnContext>, Consumer<UseOnContext>> pair = toolModifiedState == null ? null : Pair.of(ctx -> true, changeIntoState(toolModifiedState));
+        if (player != null) {
+            if (context.getClickedFace() != Direction.DOWN && player.isShiftKeyDown()) {
+                BlockState blockstate3 = blockstate.getToolModifiedState(context, ItemAbilities.SHOVEL_FLATTEN, false);
+                BlockState blockstate4;
+                if (blockstate3 != null && level.getBlockState(blockPos.above()).isAir()) {
+                    level.playSound(player, blockPos, SoundEvents.SHOVEL_FLATTEN, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    blockstate4 = blockstate3;
+                } else if ((blockstate4 = blockstate.getToolModifiedState(context, ItemAbilities.SHOVEL_FLATTEN, false)) != null ) {
+                    if (!level.isClientSide) {
+                        level.levelEvent(null, 1009, blockPos, 0);
+                    }
+                }
+                if (blockstate4 != null) {
+                    if (!level.isClientSide) {
+                        level.setBlock(blockPos, blockstate4, 11);
+                        level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, blockstate4));
+                        context.getItemInHand().hurtAndBreak(1, player, LivingEntity.getSlotForHand(context.getHand()));
+                    } else {
+                        return InteractionResult.PASS;
+                    }
+                    return InteractionResult.sidedSuccess(level.isClientSide);
+                }
+            } else if (pair != null && !player.isShiftKeyDown()) {
+                Predicate<UseOnContext> predicate = pair.getFirst();
+                Consumer<UseOnContext> contextConsumer = pair.getSecond();
+                if (predicate.test(context)) {
+                    level.playSound(player, blockPos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    if (!level.isClientSide) {
+                        contextConsumer.accept(context);
+                        context.getItemInHand().hurtAndBreak(1, player, LivingEntity.getSlotForHand(context.getHand()));
+                    }
+                    return InteractionResult.sidedSuccess(level.isClientSide);
+                }
+                return InteractionResult.PASS;
+            } else if (!playerHasShieldUseIntent(context)) {
+                Optional<BlockState> optional = this.evaluateNewBlockState(level, blockPos, player, level.getBlockState(blockPos), context);
+                if (optional.isEmpty()) {
+                    return InteractionResult.PASS;
+                } else {
+                    ItemStack itemStack = context.getItemInHand();
+                    if (player instanceof ServerPlayer) {
+                        CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer) player, blockPos, itemStack);
+                    }
+                    level.setBlock(blockPos, optional.get(), 11);
+                    level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, optional.get()));
+                    itemStack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(context.getHand()));
+                    return InteractionResult.sidedSuccess(level.isClientSide);
+                }
+            } else if (blockstate1 != null) {
+                ItemStack itemStack = context.getItemInHand();
+                if (player instanceof ServerPlayer) {
+                    CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer) player, blockPos, itemStack);
+                    itemStack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(context.getHand()));
+                    return InteractionResult.sidedSuccess(level.isClientSide);
+                }
+            }
+        }
+        return InteractionResult.PASS;
+    }
+
+    public static Consumer<UseOnContext> changeIntoState(BlockState state) {
+        return p_316061_ -> {
+            p_316061_.getLevel().setBlock(p_316061_.getClickedPos(), state, 11);
+            p_316061_.getLevel().gameEvent(GameEvent.BLOCK_CHANGE, p_316061_.getClickedPos(), GameEvent.Context.of(p_316061_.getPlayer(), state));
+        };
+    }
+
+    private static boolean playerHasShieldUseIntent(UseOnContext context) {
+        Player player = context.getPlayer();
+        return context.getHand().equals(InteractionHand.MAIN_HAND) && player.getOffhandItem().is(Items.SHIELD) && !player.isSecondaryUseActive();
+    }
+
+    private Optional<BlockState> evaluateNewBlockState(Level level, BlockPos pos, @Nullable Player player, BlockState state, UseOnContext p_40529_) {
+        Optional<BlockState> optional = Optional.ofNullable(state.getToolModifiedState(p_40529_, ItemAbilities.AXE_STRIP, false));
+        if (optional.isPresent()) {
+            level.playSound(player, pos, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1.0F, 1.0F);
+            return optional;
+        } else {
+            Optional<BlockState> optional1 = Optional.ofNullable(state.getToolModifiedState(p_40529_, ItemAbilities.AXE_SCRAPE, false));
+            if (optional1.isPresent()) {
+                level.playSound(player, pos, SoundEvents.AXE_SCRAPE, SoundSource.BLOCKS, 1.0F, 1.0F);
+                level.levelEvent(player, 3005, pos, 0);
+                return optional1;
+            } else {
+                Optional<BlockState> optional2 = Optional.ofNullable(state.getToolModifiedState(p_40529_, ItemAbilities.AXE_WAX_OFF, false));
+                if (optional2.isPresent()) {
+                    level.playSound(player, pos, SoundEvents.AXE_WAX_OFF, SoundSource.BLOCKS, 1.0F, 1.0F);
+                    level.levelEvent(player, 3004, pos, 0);
+                    return optional2;
+                } else {
+                    return Optional.empty();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean canPerformAction(ItemStack stack, ItemAbility itemAbility) {
+        return ItemAbilities.DEFAULT_AXE_ACTIONS.contains(itemAbility) || ItemAbilities.DEFAULT_HOE_ACTIONS.contains(itemAbility) || ItemAbilities.DEFAULT_SHEARS_ACTIONS.contains(itemAbility) || ItemAbilities.DEFAULT_SHOVEL_ACTIONS.contains(itemAbility);
     }
 }
