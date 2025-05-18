1a30274621befd625d8ff9d5d572c4dd91301501
kisaraginoah <kouta19501821@gmail.com>
2025-05-19 08:24:20 +0900

    プレイヤーを除外

--- src/main/java/com/kisaraginoah/atamanikita/event/LastDamageEvent.java
+++ src/main/java/com/kisaraginoah/atamanikita/event/LastDamageEvent.java
@@ -1,6 +1,7 @@
 package com.kisaraginoah.atamanikita.event;
 
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
@@ -9,7 +10,7 @@ import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 public class LastDamageEvent {
     @SubscribeEvent
     public static void onEntityHurt(LivingDamageEvent.Post event) {
-        if (event.getSource().getEntity() instanceof LivingEntity) {
+        if (event.getSource().getEntity() instanceof LivingEntity && !(event.getSource().getEntity() instanceof Player)) {
             event.getEntity().getPersistentData().putFloat("LastDamageTaken", event.getNewDamage());
         }
     }
