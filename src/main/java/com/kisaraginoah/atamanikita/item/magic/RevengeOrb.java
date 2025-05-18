9ce46a5e6748e1c187402050977ac1f6d18d26e9
kisaraginoah <kouta19501821@gmail.com>
2025-05-19 08:27:45 +0900

    使用時にノックバックを発生

--- src/main/java/com/kisaraginoah/atamanikita/item/magic/RevengeOrb.java
+++ src/main/java/com/kisaraginoah/atamanikita/item/magic/RevengeOrb.java
@@ -51,33 +51,42 @@ public class RevengeOrb extends Item {
                     player.sendSystemMessage(Component.translatable("item.atamanikita.revenge_orb.fail"));
                     player.getCooldowns().addCooldown(stack.getItem(), 100);
                     return super.finishUsingItem(stack, level, livingEntity);
-                }
-                for(Entity entity : EntityUtils.getNearbyEntities(player, 5)) {
-                    if (entity instanceof LivingEntity target) {
-                        target.hurt(player.damageSources().playerAttack(player), player.getPersistentData().getFloat("LastDamageTaken") * 2F);
-                        level.playSound(target, target.blockPosition(), SoundEvents.BEACON_ACTIVATE, SoundSource.PLAYERS, 1.0F, 1.0F);
-                        if (level instanceof ServerLevel serverLevel) {
-                            AABB aabb = entity.getBoundingBox().inflate(0.2);
-                            RandomSource random = serverLevel.getRandom();
-
-                            for (int i = 0; i < 20; i++) {
-                                double x = aabb.minX + (aabb.maxX - aabb.minX) * random.nextDouble();
-                                double y = aabb.minY + (aabb.maxY - aabb.minY) * random.nextDouble();
-                                double z = aabb.minZ + (aabb.maxZ - aabb.minZ) * random.nextDouble();
-
-                                serverLevel.sendParticles(ParticleTypes.FIREWORK, x, y, z, 1, 0, 0.01, 0, 0);
+                } else {
+                    for(Entity entity : EntityUtils.getNearbyEntities(player, 5)) {
+                        if (entity instanceof LivingEntity target) {
+                            target.hurt(player.damageSources().playerAttack(player), player.getPersistentData().getFloat("LastDamageTaken") * 2F);
+                            double dx = player.getX() - target.getX();
+                            double dz = player.getZ() - target.getZ();
+                            double distance = Math.sqrt(dx * dx + dz * dz);
+                            if (distance != 0) {
+                                dx /= distance;
+                                dz /= distance;
+                                target.knockback(1.0F, dx, dz);
                             }
-                            for (int i = 0; i < 20; i++) {
-                                double x = aabb.minX + (aabb.maxX - aabb.minX) * random.nextDouble();
-                                double y = aabb.minY + (aabb.maxY - aabb.minY) * random.nextDouble();
-                                double z = aabb.minZ + (aabb.maxZ - aabb.minZ) * random.nextDouble();
+                            level.playSound(target, target.blockPosition(), SoundEvents.BEACON_ACTIVATE, SoundSource.PLAYERS, 1.0F, 1.0F);
+                            if (level instanceof ServerLevel serverLevel) {
+                                AABB aabb = entity.getBoundingBox().inflate(0.2);
+                                RandomSource random = serverLevel.getRandom();
 
-                                serverLevel.sendParticles(ParticleTypes.CRIT, x, y, z, 1, 0, 0.01, 0, 0);
+                                for (int i = 0; i < 20; i++) {
+                                    double x = aabb.minX + (aabb.maxX - aabb.minX) * random.nextDouble();
+                                    double y = aabb.minY + (aabb.maxY - aabb.minY) * random.nextDouble();
+                                    double z = aabb.minZ + (aabb.maxZ - aabb.minZ) * random.nextDouble();
+
+                                    serverLevel.sendParticles(ParticleTypes.FIREWORK, x, y, z, 1, 0, 0.01, 0, 0);
+                                }
+                                for (int i = 0; i < 20; i++) {
+                                    double x = aabb.minX + (aabb.maxX - aabb.minX) * random.nextDouble();
+                                    double y = aabb.minY + (aabb.maxY - aabb.minY) * random.nextDouble();
+                                    double z = aabb.minZ + (aabb.maxZ - aabb.minZ) * random.nextDouble();
+
+                                    serverLevel.sendParticles(ParticleTypes.CRIT, x, y, z, 1, 0, 0.01, 0, 0);
+                                }
                             }
                         }
                     }
+                    player.getCooldowns().addCooldown(stack.getItem(), 200);
                 }
-                player.getCooldowns().addCooldown(stack.getItem(), 200);
             } else {
                 player.sendSystemMessage(Component.translatable("item.atamanikita.revenge_orb.fail1"));
                 player.getCooldowns().addCooldown(stack.getItem(), 100);
