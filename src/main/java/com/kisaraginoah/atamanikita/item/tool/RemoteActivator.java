1387cd18d584b206e474c66c5063edfa373f23b3
kisaraginoah <kouta19501821@gmail.com>
2025-05-28 08:39:13 +0900

    問題修正

--- src/main/java/com/kisaraginoah/atamanikita/item/tool/RemoteActivator.java
+++ src/main/java/com/kisaraginoah/atamanikita/item/tool/RemoteActivator.java
@@ -67,6 +67,7 @@ public class RemoteActivator extends Item {
                 } else {
                     offhandItem.useOn(new UseOnContext(level, player, InteractionHand.MAIN_HAND, offhandItem, hit));
                     state = level.getBlockState(pos);
+                    mainHandItem.remove(ModDataComponents.STATE_WITH_POS);
                     mainHandItem.set(ModDataComponents.STATE_WITH_POS, new StateWithPos(pos, state));
                 }
                 player.getCooldowns().addCooldown(this, 10);
@@ -81,13 +82,14 @@ public class RemoteActivator extends Item {
     @Override
     public InteractionResult useOn(UseOnContext context) {
         Player player = context.getPlayer();
-        if (player == null || !context.getLevel().isClientSide) {
+        if (player == null || !context.getLevel().isClientSide || !player.isShiftKeyDown()) {
             return InteractionResult.PASS;
         }
-        if (player.isShiftKeyDown() && context.getHand() != InteractionHand.OFF_HAND) {
+        if (context.getHand() != InteractionHand.OFF_HAND) {
             BlockPos pos = context.getClickedPos();
             BlockState state = context.getLevel().getBlockState(pos);
             ItemStack stack = context.getItemInHand();
+            stack.remove(ModDataComponents.STATE_WITH_POS);
             stack.set(ModDataComponents.STATE_WITH_POS, new StateWithPos(pos, state));
 
             player.displayClientMessage(
