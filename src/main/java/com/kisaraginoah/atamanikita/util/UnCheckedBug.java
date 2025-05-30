eda0e3bdedebc52709c366626104c16df3c8c8e8
kisaraginoah <kouta19501821@gmail.com>
2025-05-30 15:54:05 +0900

    更新

--- src/main/java/com/kisaraginoah/atamanikita/util/UnCheckedBug.java
+++ src/main/java/com/kisaraginoah/atamanikita/util/UnCheckedBug.java
@@ -1,10 +1,10 @@
 package com.kisaraginoah.atamanikita.util;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+/**
+ * バグ等の検証をしていないアイテムに対して未検証のツールチップの追加をします。
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface UnCheckedBug {
